063bf393e235abce4de4d2a3bbac6046ddc6522f
naviat-dev <sriemmanuel787@outlook.com>
2026-01-23 16:53:17 -0800

    Remove obsolete config items

--- Scone/Config.cs
+++ Scone/Config.cs
@@ -2,8 +2,6 @@ namespace Scone;
 
 public struct Config
 {
-	public string? fgdataPath = "C:\\Users\\sriem\\Documents\\Aviation\\fgdata\\fgdata_2024_1";
-	public string? fgelevPath = "C:\\Program Files\\FlightGear 2024.1\\bin\\fgelev.exe";
 	public string? OutputDirectory = "C:\\Users\\sriem\\Documents\\Aviation\\Scenery";
 	public int DarkMode = 2;
 
