591610cc838163f574a48497c03074ec59da2012
naviat-dev <sriemmanuel787@outlook.com>
2026-01-28 20:46:41 -0800

    Restructure AcBuilder class

--- Scone/AcBuilder.cs
+++ Scone/AcBuilder.cs
@@ -2,41 +2,64 @@ using System.Numerics;
 
 namespace Scone;
 
-public class AcObject
+public class AcBuilder
 {
-	public ulong Kids { get; private set; } = 0;
-	public string Name { get; private set; } = "";
-	public List<AcWorld> Worlds { get; private set; } = [];
+	public string Header { get; private set; } = "AC3Db";
+	public List<Material> Materials { get; private set; } = [];
+	public List<Object> Objects { get; private set; } = [];
 
-	public AcObject() { }
-	public AcObject(string name)
+	public AcBuilder() { }
+
+	public Material AddMaterial()
 	{
-		Name = name;
+		Material mat = new();
+		Materials.Add(mat);
+		return mat;
 	}
-}
 
-public class AcWorld : AcObject
-{
-	public AcWorld() { }
-}
+	public Object AddObject(string name)
+	{
+		Object obj = new(name);
+		Objects.Add(obj);
+		return obj;
+	}
 
-public class AcPoly : AcObject
-{
-	
-}
+	public class Object
+	{
+		public ulong Kids { get; private set; } = 0;
+		public string Name { get; private set; } = "";
+		public List<World> Worlds { get; private set; } = [];
 
-public class AcGroup : AcObject
-{
-	
-}
+		public Object() { }
+		public Object(string name)
+		{
+			Name = name;
+		}
+	}
 
-public struct AcMaterial
-{
-	public string? Name;
-	public Vector3 Rgb;
-	public Vector3 Ambient;
-	public Vector3 Emissive;
-	public Vector3 Specular;
-	public float Shininess;
-	public float Transparency;
+	public class World : Object
+	{
+		public World() { }
+	}
+
+	public class Poly : Object
+	{
+
+	}
+
+	public class Group : Object
+	{
+
+	}
+
+	public struct Material
+	{
+		public string? Name;
+		public Vector3 Rgb;
+		public Vector3 Ambient;
+		public Vector3 Emissive;
+		public Vector3 Specular;
+		public float Shininess;
+		public float Transparency;
+	}
 }
