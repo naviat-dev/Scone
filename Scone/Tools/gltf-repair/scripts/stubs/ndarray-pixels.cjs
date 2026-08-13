e05182865595e070f2f46644f4e8c524032fda86
naviat-dev <73054858+naviat-dev@users.noreply.github.com>
2026-08-13 14:24:43 -0700

    Add missed file

---
+++ Scone/Tools/gltf-repair/scripts/stubs/ndarray-pixels.cjs
@@ -0,0 +1,14 @@
+"use strict";
+
+async function getPixels() {
+  throw new Error("ndarray-pixels is not available in the packaged gltf-repair binary.");
+}
+
+async function savePixels() {
+  throw new Error("ndarray-pixels is not available in the packaged gltf-repair binary.");
+}
+
+module.exports = {
+  getPixels,
+  savePixels,
+};
