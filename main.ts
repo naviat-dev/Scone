8ec3c7a8d79f5b5a8d9d30f64101c6832ef9dd78
naviat-dev <73054858+naviat-dev@users.noreply.github.com>
2026-08-28 15:08:55 -0700

    General cleanup and formatting

--- main.ts
+++ main.ts
@@ -3,28 +3,28 @@ import { config, loadConfig, saveConfig } from './config.js';
 import { convertScenery } from './converter.js';
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600	
+	})
 
-  win.loadFile('index.html')
+	win.loadFile('index.html')
 }
 
 app.whenReady().then(async () => {
-  await loadConfig();
-  createWindow()
+	await loadConfig();
+	createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-  convertScenery('/home/israel-emmanuel/Documents/Aviation/scone-packs/neptune-ljbl_1.0.2_jdesn', '/home/israel-emmanuel/Documents/Scone/Output', true, false);
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
+	convertScenery('/home/israel-emmanuel/Documents/Aviation/scone-packs/neptune-ljbl_1.0.2_jdesn', '/home/israel-emmanuel/Documents/Scone/Output', true, false);
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
